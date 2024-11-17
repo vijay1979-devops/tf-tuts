7469e159327f51ed6d8395db909026db13be6cf9
vijayakumar annamalai <vijay.devops.bha@gmail.com>
2024-11-17 08:45:07 +0530

    modify

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "my_vm" {
- ami           = var.ami //Ubuntu AMI
- instance_type = var.instance_type
+  ami           = var.ami //Ubuntu AMI
+  instance_type = var.instance_type
 
- tags = {
-   Name = var.name_tag,
- }
+  tags = {
+    Name = var.name_tag,
+  }
 }
 
